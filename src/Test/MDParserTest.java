64afbe2ccf04f4bcc1942fb2ab37bf57a8c1ffae
YummyPotatoPie <1397116@bsu.edu.ru>
2021-04-14 11:31:51 +0300

    Added new test

--- src/Test/MDParserTest.java
+++ src/Test/MDParserTest.java
@@ -7,14 +7,28 @@ import org.junit.Test;
 public class MDParserTest extends MDParser {
 
     @Test
-    public void parseTest() {
+    public void parseFirstTest() {
 
         String[] expected = new String[] {
                 "####", "Hello", "######Nothing", "here", "text", "text", "text",
                 "text", "text", "text", "text", "text", "text", "text", "***hello***"
         };
 
-        String[] actual = parse("./src/Test/MarkdownTestFiles/ParseTest1.md");
+        String[] actual = parse("src/Test/MarkdownTestFiles/ParseTest1.md");
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void parseSecondTest() {
+
+        String[] expected = new String[] {
+                "###################", "Test", "TEST", "![Test]", "(Test/TEST/TESt/TESTimage.test)",
+                "![Test](Test/TEST/TESt/TESTimage.test)", "[Test]", "**_asdj_**", "test", "test",
+                "TEST", "TEST", "TEST", "TEST", "TEST"
+        };
+
+        String[] actual = parse("src/Test/MarkdownTestFiles/ParseTest2.md");
 
         Assert.assertArrayEquals(expected, actual);
     }
