91687ab4a9b028c04bab06a26b83f5981797f72b
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-15 12:56:00 +0300

    Added MDTokenMaker class

---
+++ src/MarkdownToHTML/MDTokenMaker.java
@@ -0,0 +1,14 @@
+package MarkdownToHTML;
+
+import MarkdownToHTML.TokenMakerFSA.*;
+
+class MDTokenMaker extends Handler {
+
+    private Strategy currentFSA;
+
+    @Override
+    public Object handle(Object request) {
+        return new Object();
+    }
+
+}
