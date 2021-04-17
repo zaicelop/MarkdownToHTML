f57123b7e2741479a642d02b4d51b579dce07e08
Перегудин Никита <zaicelop1990@gmail.com>
2021-04-17 10:55:38 +0300

    Class Request

---
+++ src/MarkdownToHTML/Request.java
@@ -0,0 +1,10 @@
+package MarkdownToHTML;
+
+public class Request {
+    public string[] data;
+    public void setData(){}
+    public Request() {}
+    public Request(string[] data){
+        Request request = new Request("Java")
+    }
+}
