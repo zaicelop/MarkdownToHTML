70dee20374a937cdb3bc1dac29edbc9c52edd043
Перегудин Никита <zaicelop1990@gmail.com>
2021-04-17 11:06:50 +0300

    Class Request part two

--- src/MarkdownToHTML/Request.java
+++ src/MarkdownToHTML/Request.java
@@ -1,10 +1,12 @@
 package MarkdownToHTML;
 
 public class Request {
-    public string[] data;
-    public void setData(){}
+    public String[] data;
+    public void setData(String[] arr){
+        this.data = arr;
+    }
     public Request() {}
-    public Request(string[] data){
+    public Request(String[] data){
         Request request = new Request("Java")
     }
 }
